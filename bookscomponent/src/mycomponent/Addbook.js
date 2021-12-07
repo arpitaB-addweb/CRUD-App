e83128a80cd9dabe82e65c82d1315fe8eababa98
arpitaB-addweb <95284107+arpitaB-addweb@users.noreply.github.com>
2021-12-07 18:08:01 +0530

    Update Addbook.js

--- bookscomponent/src/mycomponent/Addbook.js
+++ bookscomponent/src/mycomponent/Addbook.js
@@ -1,8 +1,54 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios'
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
+
 
 const AddUser = () => {
     // let history = useHistory();
+
+    const validationSchema = Yup.object().shape({
+       
+        name: Yup.string()
+            .required('Name is required'),
+        username: Yup.string()
+            .required("UserName is required"),
+        email: Yup.string()
+            .required('Email is required')
+            .email('Email is invalid'),
+        street: Yup.string()
+            .required("Address is required field"),
+        suite: Yup.string()
+            .required("Address is required field"),
+        city: Yup.string()
+            .required("Address is required field"),
+        zipcode: Yup.number()
+            .required("Address is required field"),
+        phone: Yup.number()
+            .typeError("That doesn't look like a phone number")
+            .positive("A phone number can't start with a minus")
+            .integer("A phone number can't include a decimal point")
+            .min(10)
+            .required('A phone number is required'),
+        lat: Yup.number()
+            .required(),
+        lag: Yup.number()
+            .required(),
+        website: Yup.string()
+            .required('It is required'),
+        cname: Yup.string()
+            .required("Please add this!!"),
+        catchPhrase: Yup.string()
+            .required("Please add this!!"),
+        bs: Yup.string()
+            .required("Please add this!!")
+    });
+    const formOptions = { resolver: yupResolver(validationSchema) };
+
+    const { register, handleSubmit, reset , formState } = useForm(formOptions);
+    const { errors } = formState;
 
 
     const [error, setError] = useState(null);
@@ -13,8 +59,8 @@ const AddUser = () => {
         setUser({ ...user, [e.target.name]: e.target.value });
     };
     
-    const onSubmit = async e => {
-        e.preventDefault();
+    const onSubmit = async (data) => {
+        // e.preventDefault()
         const add = {
             name : user.name,
             username : user.username,
@@ -37,8 +83,9 @@ const AddUser = () => {
                 bs: user.bs
             }
         }
-        console.log(add);
-        await axios.post("http://localhost:3005/users", add);
+        alert(JSON.stringify(data));
+        console.log(data);
+         await axios.post("http://localhost:3005/users", data);
         // history.push("/");
     };
 
@@ -68,7 +115,7 @@ const AddUser = () => {
             <div className="container">
                 <div className="main">
                     <h1>AddUser</h1>
-                    <form onSubmit={e => onSubmit(e)}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <div className="form-group">
                             <input
                                 type="text"
@@ -77,7 +124,10 @@ const AddUser = () => {
                                 name="name"
                                 value={user.name}
                                 onChange={onInputChange}
+
+                                {...register('name')} className={`${errors.name ? 'is-invalid' : ''}`} 
                             />
+                             <div className="invalid-feedback">{errors.name?.message}</div>
                         </div>
                         <div className="form-group">
                             <input
@@ -87,7 +137,10 @@ const AddUser = () => {
                                 name="username"
                                 value={user.username}
                                 onChange={onInputChange}
+
+                                {...register('username')} className={`${errors.username ? 'is-invalid' : ''}`} 
                             />
+                               <div className="invalid-feedback">{errors.username?.message}</div>
                         </div>
                         <div className="form-group">
                             <input
@@ -97,7 +150,10 @@ const AddUser = () => {
                                 name="email"
                                 value={user.email}
                                 onChange={onInputChange}
+
+                                {...register('email')} className={`${errors.email ? 'is-invalid' : ''}`} 
                             />
+                            <div className="invalid-feedback">{errors.email?.message}</div>
                         </div>
                         <div className="form-group">
                             <p>Address</p>
@@ -108,7 +164,9 @@ const AddUser = () => {
                                 name="street"
                                 value={user.street}
                                 onChange={onInputChange}
+                                {...register('street')} className={`${errors.street ? 'is-invalid' : ''}`} 
                             />
+                              <div className="invalid-feedback">{errors.street?.message}</div>
                              <input
                                 type="text"
                                 className="address"
@@ -116,7 +174,9 @@ const AddUser = () => {
                                 name="suite"
                                 value={user.suite}
                                 onChange={onInputChange}
+                                {...register('suite')} className={`${errors.suite ? 'is-invalid' : ''}`} 
                             />
+                              <div className="invalid-feedback">{errors.suite?.message}</div>
                              <input
                                 type="text"
                                 className="address"
@@ -124,7 +184,10 @@ const AddUser = () => {
                                 name="city"
                                 value={user.city}
                                 onChange={onInputChange}
+
+                                {...register('city')} className={`${errors.city ? 'is-invalid' : ''}`} 
                             />
+                              <div className="invalid-feedback">{errors.city?.message}</div>
                             <input
                                 type="text"
                                 className="address"
@@ -132,7 +195,10 @@ const AddUser = () => {
                                 name="zipcode"
                                 value={user.zipcode}
                                 onChange={onInputChange}
+
+                                {...register('zipcode')} className={`${errors.zipcode ? 'is-invalid' : ''}`} 
                             />
+                              <div className="invalid-feedback">{errors.zipcode?.message}</div>
                             <p>Geo</p>
                             <input
                                 type="text"
@@ -141,7 +207,10 @@ const AddUser = () => {
                                 name="lat"
                                 value={user.lat}
                                 onChange={onInputChange}
-                            />
+
+                                {...register('lat')} className={`${errors.lat ? 'is-invalid' : ''}`} 
+                                />
+                                  <div className="invalid-feedback">{errors.lat?.message}</div>
                             <input
                                 type="text"
                                 className="address"
@@ -149,7 +218,10 @@ const AddUser = () => {
                                 name="lag"
                                 value={user.lag}
                                 onChange={onInputChange}
+
+                                {...register('lag')} className={`${errors.lag ? 'is-invalid' : ''}`} 
                             />
+                                  <div className="invalid-feedback">{errors.lag?.message}</div>
                         </div>
                         <div className="form-group">
                             <input
@@ -159,7 +231,10 @@ const AddUser = () => {
                                 name="phone"
                                 value={user.phone}
                                 onChange={onInputChange}
+                                {...register('phone')} className={`${errors.phone ? 'is-invalid' : ''}`} 
                             />
+                                  <div className="invalid-feedback">{errors.phone?.message}</div>
+                            
                         </div>
                         <div className="form-group">
                             <input
@@ -169,7 +244,10 @@ const AddUser = () => {
                                 name="website"
                                 value={user.website}
                                 onChange={onInputChange}
-                            />
+
+                                {...register('website')} className={`${errors.website ? 'is-invalid' : ''}`} 
+                                />
+                                  <div className="invalid-feedback">{errors.website?.message}</div>
                         </div>
                         <div className="form-group">
                             <p>Company-Details</p>
@@ -180,7 +258,10 @@ const AddUser = () => {
                                 name="cname"
                                 value={user.cname}
                                 onChange={onInputChange}
-                            />
+                                {...register('cname')} className={`${errors.cname ? 'is-invalid' : ''}`} 
+                                />
+                                  <div className="invalid-feedback">{errors.cname?.message}</div>
+                            
                             <input
                                 type="text"
                                 className="C-Name"
@@ -188,7 +269,10 @@ const AddUser = () => {
                                 name="catchPhrase"
                                 value={user.catchPhrase}
                                 onChange={onInputChange}
-                            />
+                                {...register('catchPhrase')} className={`${errors.catchPhrase ? 'is-invalid' : ''}`} 
+                                />
+                                  <div className="invalid-feedback">{errors.catchPhrase?.message}</div>
+                            
                             <input
                                 type="text"
                                 className="C-Name"
@@ -196,7 +280,10 @@ const AddUser = () => {
                                 name="bs"
                                 value={user.bs}
                                 onChange={onInputChange}
-                            />
+                                {...register('bs')} className={`${errors.bs ? 'is-invalid' : ''}`} 
+                                />
+                                  <div className="invalid-feedback">{errors.bs?.message}</div>
+                            
                         </div>
                         <button className="btn-btn">Submit</button>
                     </form>
